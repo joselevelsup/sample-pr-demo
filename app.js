f88108b0938f2eb24d3f7fe1e7d469b726f37944
Wren Purcell <purcell856@gmail.com>
2023-02-28 18:37:24 -0500

    added route

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.get("/mars", function (_req, res) {
   });
 });
 
+app.get("/mercury", function (_req, res) {
+  res.status(200).json({
+    message: "hello mercury",
+  });
+});
+
 app.listen(8080, function () {
   console.log("listening on port 8080");
 });
